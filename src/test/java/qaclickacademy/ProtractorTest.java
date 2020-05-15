34086561b2f892800b6707d3454e9b617d30b7b6
Prathamesh <prathameshd1990@yahoo.in>
2020-05-15 12:27:15 +0530

    final commit by European testing team

---
+++ src/test/java/qaclickacademy/ProtractorTest.java
@@ -0,0 +1,16 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class ProtractorTest {
+	
+	@Test
+	public void browserJavascript() {
+		System.out.println("JavaScript 1 running");
+	}
+	@Test
+	public void browserJavascript1() {
+		System.out.println("JavaScript 2 running");
+	}
+
+}
